5ea0b41629badfd991052550660e8444780eb451
kunalpatel <kunalpatel446@gmail.com>
2016-09-11 03:26:58 -0400

    updateFixAttempt

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var db = MongoClient.connect("penn_db");
 //get random from 1 to 100000
 //return "num : {x,y}" from mongo
 
-var num =  (Math.floor(Math.random() * (100000 - 1001 + 1)) + 1001).toString();
+var num;
 var url = 'mongodb://ec2-54-211-134-229.compute-1.amazonaws.com/penn_db';
 var Message;
 app.get('/read', function (req, res) {
+    num =  (Math.floor(Math.random() * (100000 - 1001 + 1)) + 1001).toString()
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
         console.log ("Connection successful");
@@ -36,14 +37,15 @@ app.get('/read', function (req, res) {
 //get value of x and y from mongo
 //hash the values and update mongo
 //return new values without a read to mongo
-app.get('/update', function (req, res) {
+app.get('/update', function (db, callback) {
+    num = (Math.floor(Math.random() * (100000 - 1001 + 1)) + 1001).toString();
     MongoClient.connect(url, function(err, db) {
         assert.equal(null, err);
         console.log ("Connection successful");
-        var findDocuments = function(db, callback) {
+        var updateDocuments = function(db, callback) {
         // Get the documents collection
         var collection = db.collection('bal1');
-        collection.updateOne({'_id': num}).toArray(function(err, docs) {
+        collection.update({'_id': num}, {$set: {'_id': (Math.floor(Math.random() * (100000 - 1001 + 1)) + 1001).toString()}}).toArray(function(err, docs) {
             assert.equal(err, null);
             console.log("Found the following records");
             console.log(docs);
@@ -51,7 +53,7 @@ app.get('/update', function (req, res) {
             callback(docs);
         });
         }
-        findDocuments(db,function(){
+        updateDocuments(db,function(){
         db.close(); 
         });
     });

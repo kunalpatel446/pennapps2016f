190ad76c0e0c426168f3a423ad1be72e10f5655c
kunalpatel <kunalpatel446@gmail.com>
2016-09-10 23:55:53 -0400

    Update Index.js

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+, assert = require('assert');
 
+var db = MongoClient.connect("penn_db");
 //get random from 1 to 100000
 //return "num : {x,y}" from mongo
+
+var num =  Math.floor(Math.random() * (100000 - 1001 + 1)) + 1001;
+var url = 'mongodb://localhost:27017/penn_db';
+
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log ("COnnection successful");
+    var findDocuments = function(db, callback) {
+	// Get the documents collection
+	var collection = db.collection('bal1');
+	collection.find({'_id': 1003}).toArray(function(err, docs) {
+	    assert.equal(err, null);
+	    console.log("Found the following records");
+	    console.log(docs);
+	    callback(docs);
+	});
+    }
+    findDocuments(db,function(){
+	db.close();
+    });
+});
+
 app.get('/read', function (req, res) {
     res.send('Hello World');
 })
@@ -18,10 +41,8 @@ app.get('/update', function (req, res) {
 })
 
 var server = app.listen(8081, function () {
-
     var host = server.address().address
     var port = server.address().port
 
     console.log("Example app listening at http://%s:%s", host, port)
-
 })

102759d01886659c972e1564f46a3f1a97b9a1cf
kunalpatel <kunalpatel446@gmail.com>
2016-09-11 03:06:33 -0400

    Testing Message Transmit/Recieve functionality

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ var db = MongoClient.connect("penn_db");
 
 var num =  (Math.floor(Math.random() * (100000 - 1001 + 1)) + 1001).toString();
 var url = 'mongodb://ec2-54-211-134-229.compute-1.amazonaws.com/penn_db';
-
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log ("COnnection successful");
-    var findDocuments = function(db, callback) {
-	// Get the documents collection
-	var collection = db.collection('bal1');
-	collection.find({'_id': num}).toArray(function(err, docs) {
-	    assert.equal(err, null);
-	    console.log("Found the following records");
-	    console.log(docs);
-	    callback(docs);
-	});
-    }
-    findDocuments(db,function(){
-	db.close(); 
-    });
-});
-
+var Message;
 app.get('/read', function (req, res) {
-    res.send('Hello World');
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log ("Connection successful");
+        var findDocuments = function(db, callback) {
+    	// Get the documents collection
+    	var collection = db.collection('bal1');
+    	collection.find({'_id': num}).toArray(function(err, docs) {
+    	    assert.equal(err, null);
+    	    console.log("Found the following records");
+    	    console.log(docs);
+            Message = docs;
+    	    callback(docs);
+    	});
+        }
+        findDocuments(db,function(){
+    	db.close(); 
+        });
+    });
+  res.send(Message);
 })
 
 //get random from 1 to 100000
@@ -37,7 +37,25 @@ app.get('/read', function (req, res) {
 //hash the values and update mongo
 //return new values without a read to mongo
 app.get('/update', function (req, res) {
-    res.send('Hello World');
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log ("Connection successful");
+        var findDocuments = function(db, callback) {
+        // Get the documents collection
+        var collection = db.collection('bal1');
+        collection.updateOne({'_id': num}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            console.log("Found the following records");
+            console.log(docs);
+            Message = docs;
+            callback(docs);
+        });
+        }
+        findDocuments(db,function(){
+        db.close(); 
+        });
+    });
+  res.send(Message);
 })
 
 var server = app.listen(8081, function (err) {
